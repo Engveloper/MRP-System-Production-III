cb75ec7056a26d3b2f4ffc4872946d936c8a2be7
moigarm <moises.garmendia745@std.uni.edu.ni>
2021-05-23 20:14:57 -0600

    minor changes for issue #5

---
+++ PlanAgregadoData.cs
@@ -0,0 +1,12 @@
+﻿namespace LoDeProduccion
+{
+    public class PlanAgregadoData
+    {
+        private PlanAgregadoModel _pam;
+
+        public PlanAgregadoData(PlanAgregadoModel pam)
+        {
+            _pam = pam;
+        }
+    }
+}
