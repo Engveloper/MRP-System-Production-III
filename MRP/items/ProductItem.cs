bb972fb52f6fd18b085119a4251763d6255263ed
Engels Baca Sevilla <engelsbaca@spartansv.net>
2021-05-30 22:09:49 -0600

    Adding Treeview product logic

---
+++ MRP/items/ProductItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoDeProduccion.MRP.items
+{
+    public class ProductItem
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int Duration { get; set; }
+        public ProductItem ParentItem { get; set; }
+
+        public bool isMainItem()
+        {
+            return this.ParentItem == null;
+        }
+
+        public ProductItem(string name, int quantity, int duration, ProductItem parent)
+        {
+            Name = name;
+            Quantity = quantity;
+            Duration = duration;
+            ParentItem = parent;
+        }
+        
+        public ProductItem(string name, int quantity, int duration)
+        {
+            Name = name;
+            Quantity = quantity;
+            Duration = duration;
+        }
+    }
+}
---
+++ MRP/items/ProductionProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoDeProduccion.MRP.items
+{
+    class ProductionProgram
+    {
+        public ProductItem MainProductItem { get; set; }
+
+        public List<ProductItem> productItems;
+
+        public ProductionProgram()
+        {
+            productItems = new List<ProductItem>();
+        }
+
+        public void addProductItem(ProductItem newItem, ProductItem parent)
+        {
+            newItem.ParentItem = parent;
+            productItems.Add(newItem);
+        }
+
+        public void removeProductItem(ProductItem item)
+        {
+            productItems.Remove(item);
+        }
+
+
+    }
+}
